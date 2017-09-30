3eec1ea31557a18377b9c39e2639d29e6649a6c2
spooner <bil.bagpuss@gmail.com>
2017-09-30 03:01:19 +0100

    Handle the mode dropdown a bit more sensibly.

--- pixel_table/pixel_table.py
+++ pixel_table/pixel_table.py
@@ -1,6 +1,7 @@
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
 
 from .pixel_grid import PixelGrid
 from .modes.paint import Paint
@@ -11,28 +12,24 @@ class PixelTable(Widget):
     pixel_grid = ObjectProperty(None)
     mode = ObjectProperty(None)
     mode_container = ObjectProperty(None)
-    mode_dropdown = ObjectProperty(None)
     mode_button = ObjectProperty(None)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self._modes = self._mode_dropdown = None
 
         self.pixel_grid.bind(on_pixel_down=self.on_pixel_down)
         self.pixel_grid.bind(on_pixel_move=self.on_pixel_move)
         self.pixel_grid.bind(on_pixel_up=self.on_pixel_up)
 
     def setup(self):
-        modes = [m() for m in [Paint, MatrixRain]]
+        self._modes = [
+            Paint(),
+            MatrixRain(),
+        ]
+        self._mode_dropdown = ModeDropDown(self._modes, self.change_mode)
 
-        for mode in modes:
-            self._add_mode(mode)
-
-        self.change_mode(modes[0])
-
-    def _add_mode(self, mode):
-        button = Button(text=mode.NAME, size_hint_y=None, height=40)
-        button.bind(on_release=lambda btn: self.change_mode(mode))
-        self.mode_dropdown.add_widget(button)
+        self.change_mode(self._modes[0])
 
     def change_mode(self, mode):
         if self.mode is not None:
@@ -40,7 +37,6 @@ class PixelTable(Widget):
 
         self.mode = mode
         self.mode_button.text = self.mode.NAME
-        self.mode_dropdown.dismiss()
         self.mode_container.clear_widgets()
         self.mode_container.add_widget(self.mode)
         self.mode.pixel_grid = self.pixel_grid
@@ -58,3 +54,24 @@ class PixelTable(Widget):
 
     def on_pixel_up(self, sender, pixel):
         self.mode.on_pixel_up(pixel)
+
+    def open_dropdown(self):
+        self._mode_dropdown.open(self.mode_button)
+
+
+class ModeDropDown(DropDown):
+    def __init__(self, modes, change_mode, **kwargs):
+        super().__init__(**kwargs)
+
+        self._change_mode = change_mode
+        for mode in modes:
+            self._add_mode(mode)
+
+    def _add_mode(self, mode):
+        button = Button(text=mode.NAME, size_hint_y=None, height=40)
+        button.bind(on_release=lambda sender: self.mode_picked(mode))
+        self.add_widget(button)
+
+    def mode_picked(self, mode):
+        self._change_mode(mode)
+        self.dismiss()
