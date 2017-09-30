05a6e6816badb2a8cd6c199cce6f087158f9351e
spooner <bil.bagpuss@gmail.com>
2017-09-30 01:20:18 +0100

    send sensible pixel data

--- arduino/led_matrix/led_matrix.ino
+++ arduino/led_matrix/led_matrix.ino
@@ -5,15 +5,14 @@
 #define HEIGHT 16
 #define NUM_LEDS (WIDTH * HEIGHT)
 #define PIN 6
-#define BRIGHTNESS 64
-
+#define BRIGHTNESS 32
 
 CRGB leds[NUM_LEDS];  
 int r, g, b;
 int x, y, xx;
 
 void setup() {
-  FastLED.addLeds<WS2812B, PIN>(leds, NUM_LEDS);//.setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2812B, PIN>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
   Serial.begin(115200);
   FastLED.show();
