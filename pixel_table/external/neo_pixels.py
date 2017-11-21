9ae4f62cb51233294c672d38660fabe86bc37cbc
spooner <bil.bagpuss@gmail.com>
2017-11-21 16:03:38 +0000

    arduino sound logic.

--- pixel_table/external/neo_pixels.py
+++ pixel_table/external/neo_pixels.py
@@ -29,7 +29,7 @@ _logger = logging.getLogger(__name__)
 
 # NeoPixels
 LED_COUNT = 256  # Number of LED pixels.
-LED_PIN = 20  # GPIO pin connected to the pixels (must support PWM!). 18 = PWM, 20 = SPI-MOSI
+LED_PIN = 10  # GPIO pin connected to the pixels (must support PWM!). 18 = PWM, 10 = SPI-MOSI
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 5  # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
